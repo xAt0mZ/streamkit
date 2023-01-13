4de2c024c853f1dcb1ec74623c72696db7303ff4
LP B <xAt0mZ@users.noreply.github.com>
2023-01-13 20:48:13 +0100

    chore: setup vite-react-swc + tauri

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+import { invoke } from '@tauri-apps/api'
+
+// now we can call our Command!
+// Right-click the application background and open the developer tools.
+// You will see "Hello, World!" printed in the console!
+invoke('greet', { name: 'World' })
+  // `invoke` returns a Promise
+  .then((response) => console.log(response))
